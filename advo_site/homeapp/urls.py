1ab22010fe9bc317f711488fb62830de5470e5ae
codarsssss <124371600+codarsssss@users.noreply.github.com>
2023-04-19 15:06:41 +0300

    new_template

--- advo_site/homeapp/urls.py
+++ advo_site/homeapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_index
+from .views import home_index, about_us_index
 
 app_name = 'homeapp'
 
 urlpatterns = [
     path('', home_index, name='index'),
+    path('about-us/', about_us_index, name='about'),
 ]
--- advo_site/homeapp/views.py
+++ advo_site/homeapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
+import time
+import datetime
+
 
 def home_index(request: HttpRequest):
-    print(request.path)
     return render(request, 'homeapp/index.html')
+
+
+def about_us_index(request: HttpRequest):
+    return render(request, 'homeapp/about-us.html')
