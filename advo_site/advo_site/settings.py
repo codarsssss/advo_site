25f9bf20531793fd0c425c291ec30fc621a9c888
codarsssss <codarsssss@gmail.com>
2023-07-14 23:02:17 +0300

    for deploy

--- advo_site/advo_site/settings.py
+++ advo_site/advo_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from secret import KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3&b*8(7xh)z1erj76kpmiwqtklupddk%nl54*!)9znmsk!otl+'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
