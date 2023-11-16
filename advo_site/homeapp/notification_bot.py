3f7a7b58ca87e417e4ac3044c70c90273b01be11
codarsssss <codarsssss@gmail.com>
2023-11-16 19:25:53 +0300

    some fix

--- advo_site/homeapp/notification_bot.py
+++ advo_site/homeapp/notification_bot.py
@@ -3,13 +3,13 @@ from aiogram.dispatcher import Dispatcher
 from advo_site.secret import TG_KEY as TOKEN, CHAT_ID, ADMIN_ID
 
 
-# bot = Bot(token=TOKEN)
-# dp = Dispatcher(bot)
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
 
 async def send_telegram_message(message):
-    # try:
-    #     await bot.send_message(chat_id=CHAT_ID, text=message)
-    # except Exception as e:
-    #     await bot.send_message(chat_id=ADMIN_ID, text=e)
+    try:
+        await bot.send_message(chat_id=CHAT_ID, text=message)
+    except Exception as e:
+        await bot.send_message(chat_id=ADMIN_ID, text=e)
     pass
