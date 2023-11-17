56c3cc6a08ff989f6910ef93758286b5a7f9b34c
codarsssss <codarsssss@gmail.com>
2023-11-18 00:04:46 +0300

    partners models + admin logic for add partner

--- advo_site/advo_site/settings.py
+++ advo_site/advo_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'l~FkLoa91lgKpY~%KgWP@0wiq2}s%~kwEssdsvsdKr~dWsBOM8O1bct$YwLx}a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
